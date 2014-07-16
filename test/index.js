9152a08c986b3f65fef86e66315f36e6f1827d30
Raynos <raynos2@gmail.com>
2014-07-16 16:17:37 -0700

    Add a test for 100% code coverage.
    I recently landed a PR for new features without adding tests.
    This is bad. The tests & tooling should shout at me for this.
    
    cc @dfellis thanks for teaching me this trick.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,49 @@
+var test = require('assert-tap').test;
+var process = require('process');
+var path = require('path');
+var exec = require('child_process').exec;
+
 require('./uncaught.js');
 require('./shutdown.js');
-// domain last. domains are iffy and fragile.
+
+test('check coverage', function t(assert) {
+    getCoverage(function onCoverage(err, out) {
+        assert.ifError(err);
+
+        if (out) {
+            console.warn('Code coverage is not 100%');
+            console.log(out.report);
+            console.log('SUGGESTED FIX: get 100% code coverage');
+            process.exit(1);
+        } else {
+            assert.ok(true);
+            assert.end();
+        }
+    });
+});
+
+function getCoverage(cb) {
+    var cmd = path.join(__dirname, '..', 'node_modules',
+        '.bin', 'istanbul');
+    exec(cmd + ' check-coverage --statements=100', {
+        cwd: path.join(__dirname, '..')
+    }, onCmd);
+
+    function onCmd(err, stdout, stderr) {
+        if (!err) {
+            return cb(null);
+        }
+
+        var reportCmd = cmd + ' report text';
+        exec(reportCmd, {
+            cwd: path.join(__dirname, '..')
+        }, onReport);
+
+        function onReport(err2, stdout2, stderr2) {
+            cb(err2, {
+                checkCoverage: stdout + stderr,
+                report: stdout2 + stderr2
+            });
+        }
+    }
+}
